07f1abb0831a5366f50ebb8c83b9c91b255fd1c0
JoeyRussoniello <jmrusso@bu.edu>
2025-10-02 08:09:10 -0400

    Added more detail and comments

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,36 @@
 fn main() {
-    println!("Hello, world!");
-}
+    let opt1 = Some(1);
+    let opt2 = None;
+    
+    // Propagation moves the error handling up! In libraries, this 
+    // allows users to decide how they handle error
+    let sum = uses_propagation(opt1, opt2);
+    println!("{:?}", sum);
+    // For example, users may want to raise an error with expect
+    sum.expect("Oops! I unwrapped in `main`");
+    // They may want to default to some value (probably something like 0)
+    sum.unwrap_or(0); 
+    
+    // Without using propagation none of this would be possible, since the function PANICS inside the function call
+    let without_prop = uses_unwrap(opt1, opt2);
+    println!("{}", without_prop);
 
+    
+}
 /// A basic function that takes two Options, and sums them
 fn sum_options(option1: Option<i32>, option2: Option<i32>) -> i32 {
     // Do this manually using match
-    unimplemented!();
+    let value1 = match option1{
+        Some(v) => v,
+        None => panic!("Expected Some, got None")
+    };
+
+    let value2 = match option2{
+        Some(v) => v,
+        None => panic!("Expected Some, got None")
+    };
+
+    return value1 + value2;
 }
 
 fn uses_unwrap(option1: Option<i32>, option2: Option<i32>) -> i32 {
@@ -32,20 +57,24 @@ fn uses_expect(option1: Option<i32>, option2: Option<i32>) -> i32 {
     return option1.expect("Option1 was None") + option2.expect("Option2 was None");
 }
 
-
-fn uses_propagation(option1: Option<i32>, option2: Option<i32>) -> Option<i32> {
-    // Maybe we don't want this function fail immediately, instead this will just pass the option up
-    let v1 = option1?;
-    let v2 = option2?;
-    return Some(v1 + v2)
-    // Alterantively in a one liner
-    // return Some(option1? + option?)
-}
-
+/// Implementing "error propagation" manually
 fn manual_propagation(option1: Option<i32>, option2: Option<i32>) -> Option<i32> {
+    // Match both option 1 and 2
     let sum = match (option1, option2) {
+        // If Both are Some, return Some(v1 + v2)
         (Some(v1), Some(v2)) => Some(v1 + v2),
+        // Otherwise return None
         _ => None
     };
     return sum;
 }
+
+fn uses_propagation(option1: Option<i32>, option2: Option<i32>) -> Option<i32> {
+    // Cleaner implementation of propagation with the propagation operator ?
+    let v1 = option1?;
+    let v2 = option2?;
+    return Some(v1 + v2)
+    // Alterantively in a one liner
+    // return Some(option1? + option2?)
+}
+
